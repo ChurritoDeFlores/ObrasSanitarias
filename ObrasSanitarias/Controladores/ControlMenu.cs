fe1bf61ed19c82b49b496f4695839dba77e966de
Fernando Cosentino <111756125+ChurritoDeFlores@users.noreply.github.com>
2023-05-29 16:19:31 -0300

    Primer commit
    Creación de Conexión a la base de datos, Creación de clase "Licitacion". Creación de Licitaciones para consultas SQL. Creación de Controladores de Licitaciones, Menús e impresión

---
+++ ObrasSanitarias/Controladores/ControlMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObrasSanitarias.Controladores
+{
+    internal class ControlMenu
+    {
+        public void Menu()
+        {
+            bool salir = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("## Obras Sanitarias ##");
+                Console.WriteLine("1- Licitaciones.");
+                Console.WriteLine("0- Salir.");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    default: { Console.WriteLine("Opcion incorrecta"); Console.ReadKey(); } break;
+                    case 0: { salir = false; }break;
+                    case 1: { MenuLicitaciones(); }break;
+
+                }
+
+
+            }while (salir == true);
+        }
+        private void MenuLicitaciones()
+        {   
+            Console.Clear();
+            ControlLicitaciones ctrlLicitaciones = new ControlLicitaciones();
+            bool salir = true;
+            do
+            {
+                Console.WriteLine("## Licitaciones ##");
+                Console.WriteLine("1- Agregar Licitacion.");
+                Console.WriteLine("2- Mostrar Licitaciones.");
+                Console.WriteLine("0- Salir.");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    default: { Console.WriteLine("Opcion incorrecta"); Console.ReadKey(); } break;
+                    case 0: { salir = false; } break;
+                    case 1: { ctrlLicitaciones.Agregar(); } break;
+                    case 2: { ctrlLicitaciones.Imprimir(); } break;
+
+                }
+
+
+            } while (salir == true);
+        }
+    }
+}
---
+++ ObrasSanitarias/Controladores/ControlTipeo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObrasSanitarias.Controladores
+{
+    internal class ControlTipeo
+    {
+        public string NoVacio(string cadena)
+        {
+            while (cadena == "" || cadena == null)
+            {
+                Console.WriteLine("No ingreso dato, por favor intente nuevamente.");
+                cadena = Console.ReadLine();
+            }
+            return cadena;
+        }
+    }
+}
